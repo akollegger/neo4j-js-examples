14e0c29e4b6eddb54eb6b090cdcc814aa35ede52
ABK <akollegger@gmail.com>
2021-08-12 09:33:46 +0200

    demonstrate isRelationship() and new Relationship()

--- test/use-types.test.ts
+++ test/use-types.test.ts
@@ -1,6 +1,7 @@
 
 // This is only available from the non-public api...
 import {isNode, Node} from 'neo4j-driver-core';
+import {isRelationship, Relationship} from 'neo4j-driver-core';
 
 describe("neo4j-driver types", () => {
   describe("Node", () => {
@@ -25,4 +26,38 @@ describe("neo4j-driver types", () => {
     const n = new Node(identity, labels, properties)
     expect(isNode(n)).toBeTruthy();
   })
+
+  describe("Relationship", () => {
+    it("can be constructed", () => {
+      const from = 0;
+      const to = 1;
+      const identity = 3;
+      const type = "R";
+      const properties = {
+        k: "value"
+      }
+      const r = new Relationship(
+        identity, from, to,
+        type, properties)
+      expect(r.identity).toBe(identity);
+      expect(r.start).toBe(from);
+      expect(r.end).toBe(to);
+      expect(r.type).toEqual(type);
+      expect(r.properties).toMatchObject(properties);
+    })
+  })
+  it("isRelationship", () => {
+    const from = 0;
+    const to = 1;
+    const identity = 3;
+    const type = "R";
+    const properties = {
+      k: "value"
+    }
+    const r = new Relationship(
+      identity, from, to,
+      type, properties)
+    expect(isRelationship(r)).toBeTruthy();
+  })
+
 })
